cff81a346225659b8c1afd7c92c4112e1b8b1d42
Martin Rocek <martin@oculum.ink>
2018-04-27 00:14:30 +0100

    :bug: guide - missing object property

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
         <site-header></site-header>
         <!-- Views -->
         <div class="content-wrapper">
-                <site-guide @event-hideoverlay="() => { this.hideGuideOverlay() }"
+                <site-guide @event-hideoverlay="() => { hideGuideOverlay() }"
                             :visible="guide"
                 ></site-guide>
             <transition name="slide-horizontal" mode="out-in">
